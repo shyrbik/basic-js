cea1d0694afc4be5eddfef9385d26010305648a0
shyrbik <58969166+shyrbik@users.noreply.github.com>
2021-02-24 12:38:48 +0300

    fix: return correct result

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -4,10 +4,13 @@ module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
 //  минимальное число ходов, необходимое для решения головоломки, равно 2n − 1, где n — число дисков[
   let turns = Math.pow(2, disksNumber) - 1;
   let seconds = turns / turnsSpeed * 3600;
-  let result = {};
+  let result = {
+    turns, seconds
+  };
   seconds = Math.floor(seconds);
   turns = Math.floor(turns);
-  result["turns"] = turns;
-  result["seconds"] = seconds;
+  result.turns = turns;
+  result.seconds = seconds;
+  
   return result;
  };
