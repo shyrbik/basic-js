f08ef22fc66937c142762fac620deb31ddccfb6b
shyrbik <58969166+shyrbik@users.noreply.github.com>
2021-02-24 12:30:31 +0300

    fix: add comment

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+//  минимальное число ходов, необходимое для решения головоломки, равно 2n − 1, где n — число дисков[
   let turns = Math.pow(2, disksNumber) - 1;
   let seconds = turns / turnsSpeed * 3600;
   let result = {};
