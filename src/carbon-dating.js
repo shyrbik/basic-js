bbf1966a9fee4fbaab60cb9fc1069377b173f18a
shyrbik <58969166+shyrbik@users.noreply.github.com>
2021-02-26 09:10:06 +0300

    feat: solve uncorrect values

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,6 +1,6 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
+const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 
@@ -9,10 +9,16 @@ module.exports = function dateSample(sampleActivity) {
   if (typeof(sampleActivity) !== 'string') {
     return false;
   }
+  sampleActivity = parseFloat(sampleActivity);
+
+  if (sampleActivity > MODERN_ACTIVITY && numSampleActivity < 0)
+    return false;
+
 
 const koef = 0.693 / HALF_LIFE_PERIOD;
-return Math.ceil(((Math.log(MODERN_ACTIVITY / sampleActivity)) / koef));
+return Math.ceil(((Math.log(MODERN_ACTIVITY / sampleActivity) / koef)));
 
 
 
 };
+
